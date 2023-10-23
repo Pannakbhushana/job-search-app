cbff8865bdafafc819f48bdbc5b7f9f4cd762956
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2023-10-23 18:31:50 +0530

    footer added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import AllRoutes from './Components/AllRoutes';
+import Footer from './Pages/Footer';
 
 function App() {
   return (
     <div className="App">
        <Navbar/>
      <AllRoutes/>
+     <br />
+     <br />
+     <br />
+     <Footer/>
     </div>
   );
 }
