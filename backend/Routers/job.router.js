3ff7a49d2a099a18744f2427dc5b23da651fa387
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2023-10-24 04:52:49 +0530

    pagination added

--- backend/Routers/job.router.js
+++ backend/Routers/job.router.js
@@ -15,14 +15,37 @@ jobRoute.post("/add",async(req,res)=>{
 })
 
 jobRoute.get("/",async(req,res)=>{
-    const query=req.query;
-    try {
-        const data=await JobModel.find(query);
-        res.status(200).send(data);
-    } catch (error) {
-        res.status(400).send({msg:error.message});
-    }     
-})
+    
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const { companyName, role, skill, department, _id } = req.query;
+    const query = {};
+  
+    
+    if (companyName) {
+      query.companyName = companyName;
+    }
+    if (role) {
+      query.role = role;
+    }
+    if (skill) {
+      query.skill = skill;
+    }
+    if (department) {
+      query.department = department;
+    }
+    if (_id) {
+      query._id = _id;
+    }
+  
+      try {
+          const offset = (page - 1) * limit;
+          const data=await JobModel.find(query).skip(offset).limit(limit);;
+          res.status(200).send(data);
+      } catch (error) {
+          res.status(400).send({msg:error.message});
+      }     
+  })
 
 jobRoute.delete("/delete/:userID", async(req,res)=>{
     const {userID}=req.params;
